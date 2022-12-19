96dbce53bf0d2e44d9e867a62f7bc221c237aa13
Yash Desai <ytkd@Yashs-MacBook-Pro.local>
2022-12-19 13:39:58 +0000

    Day 6

--- fraud_detection.py
+++ fraud_detection.py
@@ -20,6 +20,7 @@ desc = pd.read_excel('data-dictionary.xlsx', index_col=0)
 data_features = pd.read_csv('transactions_obf.csv')
 data_labels = pd.read_csv('labels_obf.csv')
 fraud_transactions = data_labels['eventId'].tolist()
+print('-->Loading our data...')
 
 # creating the target variable "isFraud"
 
@@ -80,6 +81,7 @@ pod_dummies = pd.get_dummies(data_features['part_of_day'],prefix = 'tx_at')
 data_features = pd.concat([data_features,pod_dummies],axis=1)
 data_features = data_features.drop(columns=['transactionTime','part_of_day'])
 
+print('-->Creating a new feature called part_of_day from transactionTime to signify the part of Day...')
 
 # eventId
 
@@ -90,6 +92,8 @@ and it should not be used to train the model. Hence we drop this column from our
 """
 
 data_features = data_features.drop(columns=['eventId'])
+print('')
+print('-->Dropping eventId...')
 
 # accountNumber
 
@@ -99,11 +103,12 @@ Account Number has just 766 unique values which is 0.6% of the total values. Acc
 
 acc_freq = data_features.accountNumber.value_counts()
 acc_freq = pd.DataFrame(acc_freq)
-acc_freq["name"] = acc_freq.index[i]
+acc_freq["name"] = acc_freq.index
 acc_freq.rename(columns = {'accountNumber':'acc_freq', 'name':'accountNumber'}, inplace = True)
 data_features = data_features.merge(acc_freq, on = 'accountNumber')
 data_features = data_features.drop(columns=['accountNumber'])
-
+print('')
+print('-->Computing frequency of account numbers instead of the ids...')
 # merchantId
 
 """
@@ -112,6 +117,9 @@ Merchant Id is a unique id of the merchant and has 33327 unique values. It shoul
 Hence we drop this column from our feature set.
 
 """
+data_features = data_features.drop(columns=['merchantId'])
+print('')
+print('-->Dropping merchantId...')
 
 # mcc
 
@@ -122,10 +130,12 @@ MCC represents the merchant category code of the merchant. It specifies the type
 """
 mcc_freq = data_features.mcc.value_counts()
 mcc_freq = pd.DataFrame(mcc_freq)
-mcc_freq["mcc_code"] = mcc_freq.index[i]
+mcc_freq["mcc_code"] = mcc_freq.index
 mcc_freq.rename(columns = {'mcc':'mcc_freq', 'mcc_code':'mcc'}, inplace = True)
 data_features = data_features.merge(mcc_freq, on = 'mcc')
-
+data_features = data_features.drop(columns=['mcc'])
+print('')
+print('-->Considering the frequency of merchant category code instead of the id...')
 
 # merchantCountry
 
@@ -138,3 +148,61 @@ diffrent countries. If we were to use a one hot encoding technique to convert th
 Hence I will set a threshold value for the frequency of transactions in countries below which I will put all the countries in a separate column called 'low_freq_countries'
 """
 
+merchant_country_freq = pd.DataFrame(data_features.merchantCountry.value_counts())
+merchant_country_freq["mc"] = merchant_country_freq.index
+country_list = merchant_country_freq.loc[merchant_country_freq.merchantCountry>100,"mc"] # extracting countires whose transaction frequency is more than 100
+data_features.loc[data_features["merchantCountry"].isin(country_list)==False,"merchantCountry"]="low_freq_countires" # marking rest countries as "low_freq_countries"
+
+mc_dummies = pd.get_dummies(data_features['merchantCountry'],prefix = 'mc')
+data_features = pd.concat([data_features,mc_dummies],axis=1)
+data_features = data_features.drop(columns=['merchantCountry'])
+print('')
+print('-->Separating low frequency countries and creating dummy variables...')
+
+# merchantZip
+
+'''
+
+It is the zip code of the postal address of the merchant. This column cointains 23005 missing values and the best method is to drop the column.
+'''
+print('')
+print('The number of na values for the feature "merchantZip" is', data_features.apply(lambda x : x.isnull().sum()).to_dict().get('merchantZip'))
+print('')
+print('-->Dropping merchantZip...')
+data_features = data_features.drop(columns=['merchantZip'])
+
+
+# posEntryMode
+
+'''
+It represents the point of sale entry mode and there are just 10 distinct values hence I have used a one hot encoder. This can be done using the get_dummies function from pandas.
+'''
+
+pos_dummies = pd.get_dummies(data_features['posEntryMode'],prefix='pos_mode')
+data_features = pd.concat([data_features,pos_dummies],axis=1)
+data_features = data_features.drop(columns=['posEntryMode'])
+print('')
+print('-->Creating dummy variables for posEntryMode...')
+
+# transactionAmount
+
+"""
+This column contains a certain number of negative values which are not veyr high in magnitude. The minimum tranasction amount is "-0.15". We can either reomove these 183 values or make them zero. It wont make a huge difference if I perform any of these two methods but leaving the negative values as it is can degrade the performance of our classifier. I chose to remove the observations as -ve transaction values don't make sense and making them zero without any strong reason would not make any sense as well. As far as the skewness of this feature is concerned, I do not plan to use a parametric model 
+hence transforming the values according to a specific distribution won't make sense. 
+
+"""
+
+neg_indexes = data_features[ data_features['transactionAmount'] < 0 ].index
+data_features.drop(neg_indexes , inplace=True)
+print('')
+print('-->Removing negative transactions from transactionAmount...')
+# availableCash
+
+"""
+# This column seems to be perfectly fine and can be used in our prediction models as it is. 
+
+"""
+
+data_features.to_csv('processed_df.csv')
+print('')
+print('-->Completed Pre-processing and feature engineering...')
